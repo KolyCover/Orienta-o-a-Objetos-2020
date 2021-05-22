df71ec1f59593c9c37629937714b71aee6e04d3b
Nikoly Cover Pereira <nikoly_cover@outlook.com>
2021-05-21 23:06:34 -0300

    Create class Carta

---
+++ JogoCartas/src/Carta.java
@@ -0,0 +1,25 @@
+public class Carta {
+	private char valor;
+	private char naipe;
+	
+	public Carta(char valor, char naipe) {
+		this.valor = valor;
+		this.naipe = naipe;
+	}
+	
+	public int getValor() {
+		return valor;
+	}
+	
+	public void setValor(char valor) {
+		this.valor = valor;
+	}
+	
+	public char getNaipe() {
+		return naipe;
+	}
+	
+	public void setNaipe(char naipe) {
+		this.naipe = naipe;
+	}
+}
